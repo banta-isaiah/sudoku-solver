5d87886da2a69748b4fc69466cf8870aa2a9b99e
Isaiah Banta <banta.isaiah@gmail.com>
2018-04-23 01:09:31 -0700

    'added pringing and __init__'

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -1,11 +1,33 @@
 import math
-import set
 
 
-class Sudoku(self):
+class sudoku_solver:
+    def __init__(self):
+        self.puzzle = [[0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                       ]
     
+    def clues(self):
+        return 
+
+    def _print(self):
+        row = ''
+        for i in range(len(self.puzzle)):
+            for j in range(len(self.puzzle[i])):
+                row += str(self.puzzle[i][j]) + " "
+            print(row)
+            row = ''
+        return
 
 
 def main():
-    return
+    puzzle1 = sudoku_solver()
+    puzzle1._print()
 main()
